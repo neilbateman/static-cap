f95c37e84f251a772673ef56996000d87dc44877
Student <example@example.com>
2019-09-27 15:08:13 -0700

    created routes for pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Nav from './Components/Nav';
 import Routes from './Routes';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 
 const App = () => (
     <div className="App">
+    <BrowserRouter>
       <Nav/>
       <Routes/>
+    </BrowserRouter>
     </div>
   );
 
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Home, About, Faqs, Runners, Zombies } from './Pages';
-
-
-
+import { Route, Switch, Router } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Faqs from './Pages/Faqs';
+import Runners from './Pages/Runners';
+import Zombies from './Pages/Zombies';
 
 const Routes = () => (
   <Switch>
-  <Route exact path="/" component={Home}/>
-  <Route path="/about" component={About}/>
-  <Route path="/faq" component={Faqs}/>
-  <Route path="/runners" component={Runners}/>
-  <Route path="/zombies" component={Zombies}/>
-  </Switch>
+    <Route exact path="/" component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/faq" component={Faqs}/>
+    <Route path="/runners" component={Runners}/>
+    <Route path="/zombies" component={Zombies}/>
+    </Switch>
 );
 export default Routes;
