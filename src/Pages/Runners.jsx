13293b7d5efb5e46cc878e56902973f1abc16215
Student <example@example.com>
2019-09-27 16:26:30 -0700

    card media on runners page

--- src/Pages/Runners.jsx
+++ src/Pages/Runners.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Runners = () => (
-  <div>Runners</div>
-);
-export default Runners;
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+})
+
+export default function Runners() {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image="/assets/runners1.png"
+        title="ZombieFest runners"
+        />
+      <CardContent>Words</CardContent>
+    </Card>
+  );
+}
