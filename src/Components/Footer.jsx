8a375f4d6bd6beb64307529c6f94e392227bce12
Neil Bateman <neilbatman@gmail.com>
2019-09-29 12:11:16 -0700

    refactor footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import {Typography, Container, Grid, Link} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+
+
+const footers = [
+  {
+    title: 'Affiliates',
+    description: ['Bateman Race Timing', 'Lake Jackson Lion Club'],
+  },
+  {
+    title: 'Features',
+    description: ['Cool stuff',  'Developer stuff'],
+  },
+  {
+    title: 'Resources',
+    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+  },
+  {
+    title: 'Legal',
+    description: ['Privacy policy', 'Terms of use'],
+  },
+]
 
 const useStyles = makeStyles(theme => ({footer: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },}));
 
-export default function Footer() {  const classes = useStyles();
+export default function Footer() {
+  const classes = useStyles();
   return (
-    <footer className={classes.footer}>
-    <Typography variant="h6" align="center" gutterBottom>
-      Footer
-    </Typography>
-    <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-Footer Words!!    </Typography>
-  </footer>
+<Container maxWidth="md" component="footer" className={classes.footer}>
+  <Grid container spacing={4} justify="space-evenly">
+    {footers.map(footer => (
+      <Grid item xs={6} sm={3} key={footer.title}>
+        <Typography variant="h6" color="textPrimary" gutterBottom>
+          {footer.title}
+        </Typography>
+        <ul>
+          {footer.description.map(item => (
+            <li key={item}>
+              <Link href="#" variant="subtitle1" color="textSecondary">
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Grid>
+    ))}
+  </Grid>
+</Container>
 )}
